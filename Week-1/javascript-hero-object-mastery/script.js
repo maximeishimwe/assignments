491eca698eff9e51aa0dc70db624775e7cf3e53e
Maxime Ishimwe <mxmishimwe5@gmail.com>
2024-06-22 18:52:09 +0200

    js-hero-object-mastery: task3 and task4

--- Week-1/javascript-hero-object-mastery/script.js
+++ Week-1/javascript-hero-object-mastery/script.js
@@ -1,6 +1,6 @@
 // 1. Object Creation Basics
 
-let superhero = {
+const superhero = {
   name: "The Runner",
   secretIdentity: "designer",
   powers: ["slowing down time"],
@@ -11,13 +11,51 @@ let superhero = {
 
 superhero.usePower = function (powerName) {
   if (!this.powers.includes(powerName)) throw new Error("Power not found");
-  console.log(`${this.name} ${powerName}`);
+  console.log(`${this.name} is ${powerName}`);
 };
 
 superhero.revealIdentity = function () {
   console.log(`secret identity: ${this.secretIdentity}`);
 };
 
-superhero.usePower(superhero.powers[0]); //The Runner slowing down time
+superhero.usePower(superhero.powers[0]); //The Runner is slowing down time
 // superhero.usePower("fly"); //Throws error
 superhero.revealIdentity(); // secret identity: designer
+
+// 3. Object Constructors
+
+const Superhero = function (
+  name,
+  secretIdentity,
+  powers,
+  weakness,
+  revealIdentity
+) {
+  this.name = name;
+  this.secretIdentity = secretIdentity;
+  this.powers = powers;
+  this.weakness = weakness;
+};
+
+const flyer = new Superhero(
+  "The Flyer",
+  "secretary",
+  ["flying"],
+  ["strong gravity"]
+);
+console.log(flyer);
+
+// 4. Prototypal Inheritance
+
+Superhero.prototype.usePower = function (powerName) {
+  if (!this.powers.includes(powerName)) throw new Error("Power not found");
+  console.log(`${this.name} is ${powerName}`);
+};
+
+Superhero.prototype.revealIdentity = function () {
+  console.log(`secret identity: ${this.secretIdentity}`);
+};
+
+// We have access to usePower method even though it is not present on the object itself
+flyer.usePower("flying"); // The Flyer is flying
+flyer.revealIdentity(); // secret identity: secretary
